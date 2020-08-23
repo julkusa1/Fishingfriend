32a8fa466e66e4895261eb3fe104058eaabecf6a
Sanna Julkunen <sanna.julkunen1@gmail.com>
2020-08-23 14:08:55 +0300

    Versio 1

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';                
+import './App.css';
+import ViewList from "@material-ui/icons/ViewList";
+import TrendingUp from "@material-ui/icons/TrendingUp";
+import Settings from "@material-ui/icons/Settings";
+
+function App() {                //Nostan luokat appiin
+  return (
+    <div className="App">   
+      <Header/>
+      <div className="content">
+        <Fishcard/>
+        <Fishcard/>
+        <Fishcard/>
+      </div>
+      <Menu />
+    </div>
+  );
+}
+
+function Header(props) {          //Tässä on otsikkorivi
+  return(
+    <div className="header">
+      <h1>Fishing Friend</h1>
+    </div>
+  )
+}
+
+function Fishcard(props) {        //Kalakortin rivit
+  return (
+    <div className="fishcard">
+      <div className="fishcard__line">
+          <div className="fishcard__month">July</div>
+          <div className="fishcard__date">12</div>
+          <div className="fishcard__fish">Riversalmon</div>
+      </div>
+      <div className="fishcard__line">
+          <div className="fishcard__place">Whiteriver</div>
+          <div className="fishcard__amount">1</div>
+      </div>
+    </div>
+  );
+}
+
+function Menu(props) {
+  return (
+    <div className="menu">
+      <div className="menu__button"><ViewList htmlColor="#fff" /></div>
+      <div className="menu__button"><TrendingUp htmlColor="#fff" /></div>
+      <div className="menu__button"><Settings htmlColor="#fff" /></div>
+    </div>
+  );
+}
+
+export default App;
