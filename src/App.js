e92aca16cdae6c310af5b4b1fe35d22102e6f269
Sanna Julkunen <sanna.julkunen1@gmail.com>
2020-08-28 11:47:06 +0300

    Tehty videon mukaan, ei toimi vielä

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";                
 import './App.css';
+import testdata from "./testdata";    //Tuodaan testidata
 
 import Header from "./components/Header/Header";  //Nostetaan Header komponentti Appiin
 import List from "./components/List/List";  //Nostetaan lista Appiin
@@ -9,11 +10,19 @@ import Settings from "./components/Settings/Settings";  //Nostetaan asetukset Ap
 import Menu from "./components/Menu/Menu";    //Nostetaan menu Appiin
 
 function App() {                    //Nostetaan komponentit App funktioon reitittimellä
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: testdata
+    }
+  }
+  
   return (
     <Router> 
       <div className="App">   
         <Header/>
-        <Route path="/" exact component= {List}/>
+  <Route path="/" exact render={() => <List data={this.state.data}/> }/>
         <Route path="/stats" component= {Stats}/>
         <Route path="/settings" component= {Settings}/> 
         <Menu/>
