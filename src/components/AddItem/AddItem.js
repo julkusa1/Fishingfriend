47f9e5b44b2e8ef858103fd583daa0983fdf31a2
Sanna Julkunen <sanna.julkunen1@gmail.com>
2020-08-28 13:23:07 +0300

    AddItem ei toimi

---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Content from "../Content/Content";
+
+function AddItem(props) {
+    return (
+      <Content>
+        <h2>Add new catch</h2>
+      </Content>
+    );
+  }
+
+  export default AddItem;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 import Fishcard from "../Fishcard/Fishcard"; //Nostetaan kalakortti komponenteista
 import Content from "../Content/Content";
-import testdata from "../../testdata";    //Tuodaan testidata
+import {FloatingButton} from "../buttons";
 
 function List(props) {
 
-    let rows = testdata.map(fishinfo => {
+    let rows = props.data.map(fishinfo => {
       return (
         <Fishcard data={fishinfo}/>   //Tässä välitetään testidata kalakorteille
       );
@@ -16,6 +17,7 @@ function List(props) {
     return (
       <Content>
         {rows}
+        <Link to="/add"><FloatingButton primary>+</FloatingButton></Link>
       </Content>
     );
 }
