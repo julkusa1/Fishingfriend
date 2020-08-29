87c6eb85ce2dcd78bf463e56407de15887452f9a
Sanna Julkunen <sanna.julkunen1@gmail.com>
2020-08-29 13:53:07 +0300

    Tietojen lisääminen, ei toimi

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import Content from "../Content/Content";
-import Button from "../buttons";
+import ItemForm from "../ItemForm/ItemForm";
 
 import "./AddItem.css";
 
-function AddItem(props) {       //Tehdään lomake, jolla tietoja voi lisätä
+function AddItem(props) {       
     return (
       <Content>
 
@@ -13,43 +13,8 @@ function AddItem(props) {       //Tehdään lomake, jolla tietoja voi lisätä
 
         <h2>~~~~Add new catch~~~~</h2>
 
-        <form>
-
-            <div className="itemform">
-                <div className="itemform__row">
-                    <div>
-                        <label for="date">Date</label>
-                        <input type="text" name="date" size="10" />
-                    </div>
-                    <div>
-                        <label for="place">Location</label>
-                        <input type="text" name="place" />
-                    </div>
-                 </div>
-
-                 <div className="itemform__row">                
-                    <div>
-                        <label for="fishtype">Species</label>
-                        <input type="text" name="fishtype" />
-                    </div>
-                    <div>
-                        <label for="weight">Weight</label>
-                        <input type="text" name="weight" size="10" />
-                    </div>
-                </div>
-
-                <div className="itemform__row">
-                    <div>
-                        <Button>CANCEL</Button>
-                    </div>
-                    <div>
-                        <Button primary>ADD</Button>
-                    </div>
-                </div>
-
-             </div>
-
-        </form>
+        <ItemForm onFormSubmit={props.onFormSubmit} />
+ 
         </div>
       </Content>
     );
---
+++ src/components/ItemForm/ItemForm.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+import Button from "../buttons";
+
+import "./ItemForm.css";
+
+class ItemForm extends React.Component {    //Tässä lomake, jolla tietoja voi lisätä
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {
+                date: undefined,
+                place: undefined,
+                Species: undefined,
+                weight: 0
+            }
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {                                      //Tällä toiminnolla voidaan käsitellä lomakkeelta saatavaa tietoa
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+            data: {
+                ...this.state.data,
+                [name]: value
+            }
+        });
+      }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("send form");
+        let data = Object.assign({}, this.state.data);
+        data.weight = parceFloat(data.summa);
+        this.props.onFormSubmit(data);
+    }
+
+    render() {                              //Tässä lomakkeen syöttökentät ja valikot
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="itemform">
+                    <div className="itemform__row">
+                        <div>
+                            <label for="date">Date</label>
+                            <input type="date" name="date" value={this.state.data.date} onChange={this.handleInputChange}/>
+                        </div>
+                        <div>
+                            <label for="place">Location</label>
+                            <select name="place" value={this.state.data.place} onChange={this.handleInputChange}>
+                                <option value="Bigriver">Big river</option>
+                                <option value="Rabbit river">Rabit river</option>
+                                <option value="Whiteriver">White river</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="itemform__row">                
+                        <div>
+                            <label for="fishtype">Species</label>
+                            <select name="fishtype" value={this.state.data.fishtype} onChange={this.handleInputChange}>
+                                <option value="Pike">Grayling</option>
+                                <option value="Pike">Pike</option>
+                                <option value="Salmon">River Salmon</option>
+                                <option value="Trout">Trout</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="weight">Weight</label>
+                            <input type="number" name="weight" step="0.1" value={this.state.data.weight} onChange={this.handleInputChange}/>
+                        </div>
+                    </div>
+
+                    <div className="itemform__row">
+                        <div>
+                            <Button>CANCEL</Button>
+                        </div>
+                        <div>
+                            <Button type="submit" primary>ADD</Button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </form>
+        );
+    }
+}
+
+export default ItemForm;
